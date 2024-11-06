96f9d47bc5291c7210a56d77ddd29de4c593a5b4
Mu-Magdy <muhammedmagdy1015@gmail.com>
2024-11-06 17:19:07 +0200

    add chrome path

--- helper/scrap.py
+++ helper/scrap.py
@@ -49,6 +49,7 @@ def scraper(url, email, password):
     chrome_options.add_argument("--disable-dev-shm-usage")
     
     # Initialize the WebDriver
+    chrome_options.binary_location = "packages.txt"
     driver = webdriver.Chrome(options=chrome_options)
 
     try:
