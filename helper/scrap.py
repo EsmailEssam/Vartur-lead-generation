62f929164fd467e71e2410a96010d1970264580e
Esmail Essam <esmailessam6@gmail.com>
2024-11-07 16:24:37 +0200

    test

--- helper/scrap.py
+++ helper/scrap.py
@@ -118,6 +118,7 @@ def scraper(url, email, password):
     options = Options()
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
